d7d9a5e25e935b0421927a726dea12e196b5e659
hachicom <adinan_balves@yahoo.com.br>
2015-03-07 20:01:48 -0300

    try with tap event

--- www/js/main.js
+++ www/js/main.js
@@ -181,7 +181,7 @@ window.onload = function() {
       this.addChild(label2);
       
       // Touch listener
-      this.addEventListener(Event.TOUCH_START,this.handleTouchControl);
+      this.addEventListener(Event.TAP,this.handleTouchControl);
       // Update
       this.addEventListener(Event.ENTER_FRAME, this.update);
     },
