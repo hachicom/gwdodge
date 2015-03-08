a51330c3039f49b2547de3068d3f2754af9cd898
hachicom <adinan_balves@yahoo.com.br>
2015-03-07 23:22:55 -0300

    snif

--- www/js/main.js
+++ www/js/main.js
@@ -78,13 +78,14 @@ window.onload = function() {
     document.addEventListener("backbutton", onBackKeyDown, false);
     
     var last_click_time = new Date().getTime();
-    document.addEventListener('touchstart', function (e) {
-      click_time = e['timeStamp'];
-      if (click_time && (click_time - last_click_time) < 1000) {
-        e.stopImmediatePropagation();
+    document.addEventListener('click', function (e) {
+      console.log("cliquei");
+      // click_time = e['timeStamp'];
+      // if (click_time && (click_time - last_click_time) < 1000) {
+        // e.stopImmediatePropagation();
         e.preventDefault();
         return false;
-      }
+      //}
       last_click_time = click_time;
     }, true);
       
