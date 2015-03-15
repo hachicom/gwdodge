8dd4cfca8ca9783d56e58444e18c6b362298a00b
hachicom <adinan_balves@yahoo.com.br>
2015-03-15 19:41:52 -0300

    prod

--- www/js/main.js
+++ www/js/main.js
@@ -2,7 +2,7 @@ var keeploop = true;
 var jumpSnd;
 var isAndroid = /Android/i.test(navigator.userAgent);
 //A linha abaixo é para debug. Ao compilar, comentá-la para usar funções do Phonegap
-isAndroid = false;
+//isAndroid = false;
 
 // 1 - Start enchant.js
 enchant();
